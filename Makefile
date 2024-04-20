78f69fb25ab707518ecf6f5612e18ea626275062
Philipp Schneider <pschneid@student.42berlin.de>
2024-04-20 15:22:36 +0200

    add memcpy prototype

--- Makefile
+++ Makefile
@@ -6,14 +6,14 @@
 #    By: pschneid <pschneid@student.42berl...>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/02/18 21:20:23 by pschneid          #+#    #+#              #
-#    Updated: 2024/04/19 18:47:38 by pschneid         ###   ########.fr        #
+#    Updated: 2024/04/19 19:42:48 by pschneid         ###   ########.fr        #
 #                                                                              #
 #******************************************************************************#
 CFILES_DIR = srcs
 HEADERS	= .
 
 CFILENAMES = ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c \
-	ft_strlen.c ft_memset.c
+	ft_strlen.c ft_memset.c ft_bzero.c ft_memcpy.c ft_memmove.c
 CFILES = $(foreach C,$(CFILENAMES),$(CFILES_DIR)/$(C))
 OBJECTS = ${CFILES:.c=.o}
 
--- srcs/ft_memcpy.c
+++ srcs/ft_memcpy.c
@@ -1,27 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pschneid <pschneid@student.42berl...>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/19 17:18:11 by pschneid          #+#    #+#             */
-/*   Updated: 2024/04/20 15:20:34 by pschneid         ###   ########.fr       */
+/*   Created: 2024/04/19 19:41:18 by pschneid          #+#    #+#             */
+/*   Updated: 2024/04/20 15:20:53 by pschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#ifndef LIBFT_H
-# define LIBFT_H
 
-# include <stddef.h>
-
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *s);
-void	*ft_memset(void *s, int c, size_t n);
-void	ft_bzero(void *s, size_t n);
-void	*memcpy(void *dest, const void *src, size_t n);
-
-#endif
+void	*memcpy(void *dest, const void *src, size_t n)
+{
+	return (NULL);
+}
