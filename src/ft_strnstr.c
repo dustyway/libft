28b17efc59bb005a92615fe085ea6c460b6cc193
Philipp Schneider <pschneid@student.42berlin.de>
2024-04-26 16:04:29 +0200

    strnstr

--- src/ft_strnstr.c
+++ src/ft_strnstr.c
@@ -6,54 +6,24 @@
 /*   By: pschneid <pschneid@student.42berl...>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 18:41:49 by pschneid          #+#    #+#             */
-/*   Updated: 2024/04/21 19:50:57 by pschneid         ###   ########.fr       */
+/*   Updated: 2024/04/26 16:03:34 by pschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-/* char *ft_strnstr(const char *s, const char *find, size_t slen) */
-/* { */
-/* 	char c, sc; */
-/* 	size_t len; */
+char	*ft_strnstr(const char *s, const char *find, size_t slen)
+{
+	size_t	flen;
 
-/* 	if ((c = *find++) != '\0') { */
-/* 		len = ft_strlen(find); */
-/* 		do { */
-/* 			do { */
-/* 				if (slen-- < 1 || (sc = *s++) == '\0') */
-/* 					return (NULL); */
-/* 			} while (sc != c); */
-/* 			if (len > slen) */
-/* 				return (NULL); */
-/* 		} while (strncmp(s, find, len) != 0); */
-/* 		s--; */
-/* 	} */
-/* 	return ((char *)s); */
-/* } */
-
-/* char	*ft_strstr(char *str, char *to_find) */
-/* { */
-/* 	char	*a; */
-/* 	char	*b; */
-
-/* 	b = to_find; */
-/* 	if (*b == 0) */
-/* 		return (str); */
-/* 	while (*str != 0) */
-/* 	{ */
-/* 		if (*str == *b) */
-/* 		{ */
-/* 			a = str; */
-/* 			while (*a == *b && *b != 0) */
-/* 			{ */
-/* 				a++; */
-/* 				b++; */
-/* 			} */
-/* 			if (*b == 0) */
-/* 				return (str); */
-/* 		} */
-/* 		b = to_find; */
-/* 		str++; */
-/* 	} */
-/* 	return (NULL); */
-/* } */
+	flen = ft_strlen(find);
+	if (flen == 0)
+		return ((char *)s);
+	while (*s && flen <= slen)
+	{
+		if ((*s == *find) && (0 == ft_strncmp(s, find, flen)))
+			return ((char *)s);
+		s++;
+		slen--;
+	}
+	return (NULL);
+}
