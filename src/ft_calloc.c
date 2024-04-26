40685d6dac96491d3ef75645babc132780d4a353
Philipp Schneider <pschneid@student.42berlin.de>
2024-04-26 16:31:28 +0200

    calloc bugfix

--- src/ft_calloc.c
+++ src/ft_calloc.c
@@ -6,19 +6,22 @@
 /*   By: pschneid <pschneid@student.42berl...>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 15:09:11 by pschneid          #+#    #+#             */
-/*   Updated: 2024/04/23 15:16:04 by pschneid         ###   ########.fr       */
+/*   Updated: 2024/04/26 16:30:56 by pschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdlib.h>
 #include "libft.h"
+#include <stdint.h>
+#include <stdlib.h>
 
-void	*ft_calloc(size_t nmemb, size_t size)
+void	*ft_calloc(size_t count, size_t size)
 {
-	const size_t	n = nmemb * size;
-	char			*p;
+	void	*ptr;
 
-	p = malloc(n);
-	if (p)
-		ft_memset(p, '\0', n);
-	return (p);
+	if (size && count > (SIZE_MAX / size))
+		return (NULL);
+	ptr = malloc(count * size);
+	if (ptr == NULL)
+		return (ptr);
+	ft_bzero(ptr, size * count);
+	return (ptr);
 }
