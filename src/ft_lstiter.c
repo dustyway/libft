c30e39ae2c3efcc6b0fd04a0af25fb9955b8f6df
Philipp Schneider <pschneid@student.42berlin.de>
2024-04-26 20:43:53 +0200

    lstiter

---
+++ src/ft_lstiter.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pschneid <pschneid@student.42berl...>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/26 19:08:16 by pschneid          #+#    #+#             */
+/*   Updated: 2024/04/26 19:37:21 by pschneid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	t_list	*iter;
+
+	iter = lst;
+	while (iter)
+	{
+		f(iter->content);
+		iter = iter->next;
+	}
+}
---
+++ src/ft_lstmap.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pschneid <pschneid@student.42berl...>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/26 19:41:22 by pschneid          #+#    #+#             */
+/*   Updated: 2024/04/26 20:34:12 by pschneid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+#include <stdlib.h>
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*newl;
+
+	if (!lst || !f || !del)
+		return (NULL);
+	newl = ft_lstnew(f(lst->content));
+	newl->next = ft_lstmap(lst->next, f, del);
+	ft_lstdelone(lst, del);
+	return (newl);
+}
