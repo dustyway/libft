252914f0c318d7d7351470e9c6da89576720daa0
Philipp Schneider <pschneid@student.42berlin.de>
2024-04-26 13:54:20 +0200

    fix memcmp bug

--- src/ft_memcmp.c
+++ src/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: pschneid <pschneid@student.42berl...>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 18:01:24 by pschneid          #+#    #+#             */
-/*   Updated: 2024/04/21 18:36:51 by pschneid         ###   ########.fr       */
+/*   Updated: 2024/04/26 13:52:48 by pschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -19,14 +19,20 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	while (n > 0)
 	{
 		if (*su1 != *su2)
-		{
 			return (*su1 - *su2);
-		}
-		else if (*su1 == '\0')
-			return (0);
 		su1++;
 		su2++;
 		n--;
 	}
 	return (0);
 }
+
+/* int	ft_memcmp(const void *s1, const void *s2, size_t n) */
+/* { */
+/* 	const unsigned char	*su1, *su2; */
+
+/* 	for (su1 = s1, su2= s2; 0 < n; ++su1, ++su2, --n) */
+/* 		if (*su1 != *su2) */
+/* 			return ((*su1 < *su2) ? -1 : +1); */
+/* 	return (0); */
+/* } */
