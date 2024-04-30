153d884dd47e6fae681068df2d1bc18aa50bc1a8
Philipp Schneider <pschneid@student.42berlin.de>
2024-04-30 19:39:02 +0200

    memmove

--- src/ft_memmove.c
+++ src/ft_memmove.c
@@ -6,26 +6,50 @@
 /*   By: pschneid <pschneid@student.42berl...>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 19:41:24 by pschneid          #+#    #+#             */
-/*   Updated: 2024/04/30 18:37:28 by pschneid         ###   ########.fr       */
+/*   Updated: 2024/04/30 19:37:56 by pschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-void	*ft_memmove(void *dest, const void *src, size_t n)
-{
-	char		*d;
-	const char	*s = src;
+/* void	*ft_memmove(void *dest, const void *src, size_t n) */
+/* { */
+/* 	char		*d; */
+/* 	const char	*s = src; */
 
-	d = dest;
-	if (d < s)
-		while (n--)
-			*d++ = *s++;
-	else if (d > s)
+/* 	d = dest; */
+/* 	if (d < s) */
+/* 		while (n--) */
+/* 			*d++ = *s++; */
+/* 	else if (d > s) */
+/* 	{ */
+/* 		d += n; */
+/* 		s += n; */
+/* 		while (n--) */
+/* 			*--d = *--s; */
+/* 	} */
+/* 	return (dest); */
+/* } */
+
+/* #include "libft.h" */
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	size_t			i;
+	unsigned char	*from;
+	unsigned char	*to;
+	unsigned char	temp[len];
+
+	if (dst == NULL || src == NULL)
+		return (0);
+	i = 0;
+	from = (unsigned char *)src;
+	to = (unsigned char *)dst;
+	while (i < len)
 	{
-		d += n;
-		s += n;
-		while (n--)
-			*--d = *--s;
+		*(temp + i) = *(from + i);
+		i++;
 	}
-	return (dest);
+	while (i--)
+		*(to + i) = *(temp + i);
+	return (dst);
 }
