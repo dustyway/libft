489eee0d3e149eaf0f11748c5a3846dfcdb75551
Philipp Schneider <pschneid@student.42berlin.de>
2024-04-30 11:39:58 +0200

    ft_split

--- src/ft_split.c
+++ src/ft_split.c
@@ -6,99 +6,69 @@
 /*   By: pschneid <pschneid@student.42berl...>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/28 20:05:08 by pschneid          #+#    #+#             */
-/*   Updated: 2024/04/28 21:17:42 by pschneid         ###   ########.fr       */
+/*   Updated: 2024/04/30 11:39:44 by pschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+#include <stdio.h>
 #include <stdlib.h>
 
-char **ft_split(char const *s, char c)
+int	count_words(const char *str, char c)
 {
-	(void)s;
-	(void)c;
-	return NULL;
+	int	nwords;
+	int	i;
+
+	if (!str || *str == '\0')
+		return (0);
+	if (str[1] == '\0')
+		return (str[0] != c);
+	i = 1;
+	nwords = 0;
+	while (str[i] != 0)
+	{
+		if (str[i - 1] == c && str[i] != c)
+			nwords++;
+		i++;
+	}
+	if (str[i - 1] != c)
+		nwords++;
+	return (nwords);
 }
 
+void	next_word(const char *str, char c, int *start, int *end)
+{
+	*start = *end;
+	while (str[*start] && str[*start] == c)
+		(*start)++;
+	*end = *start;
+	while (str[*end] && str[*end] != c)
+		(*end)++;
+}
 
+char	**ft_split(char const *s, char c)
+{
+	int		nwords;
+	int		start;
+	int		end;
+	char	**tab;
+	char	**tab_iter;
 
-
-/* typedef struct s_point */
-/* { */
-/* 	int	start; */
-/* 	int	end; */
-/* }		t_point; */
-
-
-/* t_point	next_word(char *str, char *charset, int offset) */
-/* { */
-/* 	t_point	nxtw; */
-
-/* 	while (str[offset] && char_in_str(str[offset], charset)) */
-/* 		offset++; */
-/* 	nxtw.start = offset; */
-/* 	while (str[offset] && !char_in_str(str[offset], charset)) */
-/* 		offset++; */
-/* 	nxtw.end = offset; */
-/* 	return (nxtw); */
-/* } */
-
-/* int	nr_words(char *str, char *charset) */
-/* { */
-/* 	int	nwords; */
-/* 	int	i; */
-
-/* 	if (!str || *str == '\0') */
-/* 		return (0); */
-/* 	if (str[1] == '\0') */
-/* 		return (!char_in_str(str[0], charset)); */
-/* 	i = 1; */
-/* 	nwords = 0; */
-/* 	while (str[i] != 0) */
-/* 	{ */
-/* 		if (char_in_str(str[i - 1], charset) && !char_in_str(str[i], charset)) */
-/* 			nwords++; */
-/* 		i++; */
-/* 	} */
-/* 	if (!char_in_str(str[i - 1], charset)) */
-/* 		nwords++; */
-/* 	return (nwords); */
-/* } */
-
-/* void	strcpypart(char *dest, char *str, int start, int end) */
-/* { */
-/* 	int	i; */
-
-/* 	i = 0; */
-/* 	while (i < end - start) */
-/* 	{ */
-/* 		dest[i] = str[start + i]; */
-/* 		i++; */
-/* 	} */
-/* 	dest[i] = 0; */
-/* } */
-
-/* char	**ft_split(char *str, char *charset) */
-/* { */
-/* 	char	**ret; */
-/* 	t_point	nxtw; */
-/* 	int		nwords; */
-/* 	int		i; */
-
-/* 	nwords = nr_words(str, charset); */
-/* 	ret = (char **)malloc(sizeof(char *) * (nwords + 1)); */
-/* 	if (!ret) */
-/* 		return (NULL); */
-/* 	i = 0; */
-/* 	nxtw = next_word(str, charset, 0); */
-/* 	while (nxtw.start != nxtw.end) */
-/* 	{ */
-/* 		ret[i] = (char *)malloc(sizeof(char) * ((nxtw.end - nxtw.start) + 1)); */
-/* 		if (!ret[i]) */
-/* 			return (NULL); */
-/* 		strcpypart(ret[i], str, nxtw.start, nxtw.end); */
-/* 		nxtw = next_word(str, charset, nxtw.end); */
-/* 		i++; */
-/* 	} */
-/* 	ret[i] = 0; */
-/* 	return (ret); */
-/* } */
+	nwords = count_words(s, c);
+	tab = (char **)malloc(sizeof(char *) * (nwords + 1));
+	if (!tab)
+		return (NULL);
+	tab_iter = tab;
+	end = 0;
+	next_word(s, c, &start, &end);
+	while (start != end)
+	{
+		*tab_iter = malloc(sizeof(char) * ((end - start) + 1));
+		if (!*tab_iter)
+			return (NULL);
+		ft_strlcpy(*tab_iter, s + start, (end - start) + 1);
+		next_word(s, charset, &start, &end);
+		tab_iter++;
+	}
+	*tab_iter = NULL;
+	return (tab);
+}
