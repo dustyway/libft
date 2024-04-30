08319064a4a948063a88ff1179ddfc184c184384
Philipp Schneider <pschneid@student.42berlin.de>
2024-04-30 16:25:02 +0200

    ft_substr

--- src/ft_substr.c
+++ src/ft_substr.c
@@ -6,10 +6,11 @@
 /*   By: pschneid <pschneid@student.42berl...>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 16:38:18 by pschneid          #+#    #+#             */
-/*   Updated: 2024/04/30 16:14:18 by pschneid         ###   ########.fr       */
+/*   Updated: 2024/04/30 16:23:51 by pschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 static size_t	min(size_t a, size_t b)
@@ -19,16 +20,15 @@ static size_t	min(size_t a, size_t b)
 	return (b);
 }
 
-static char	*_empty_string(void)
-{
-	char	*s;
-
-	s = malloc(sizeof(char));
-	if (!s)
-		return (NULL);
-	s[0] = '\0';
-	return (s);
-}
+/* static char *_empty_string() */
+/* { */
+/* 	char *s; */
+/* 	s = malloc(sizeof(char)); */
+/* 	if (!s) */
+/* 		return (NULL); */
+/* 	s[0] = '\0'; */
+/* 	return (s); */
+/* } */
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -38,13 +38,19 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	slen = ft_strlen(s);
 	if (start >= slen)
-		return (_empty_string());
+		substrlen = 0;
 	else
 		substrlen = min(slen - start, len);
 	substr = malloc(sizeof(char) * (substrlen + 1));
 	if (!substr)
 		return (NULL);
+	substr[substrlen] = '\0';
 	if (substrlen)
 		ft_strlcpy(substr, s + start, substrlen + 1);
 	return (substr);
+}
+
+int	main(void)
+{
+	printf("%s\n", ft_substr("", 1, 1));
 }
