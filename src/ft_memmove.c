599fa295ac77e70f995f76ca50e03cbc4ff048ad
Philipp Schneider <pschneid@student.42berlin.de>
2024-04-30 18:01:12 +0200

    move

--- src/ft_memmove.c
+++ src/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: pschneid <pschneid@student.42berl...>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 19:41:24 by pschneid          #+#    #+#             */
-/*   Updated: 2024/04/30 17:53:54 by pschneid         ###   ########.fr       */
+/*   Updated: 2024/04/30 18:00:53 by pschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -30,27 +30,21 @@
 /* 	return (dest); */
 /* } */
 
-static	void	*ft_revmemcpy(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	char		*d;
-	const char	*s;
+	const char	*s = src;
 
-	if (!dest || !src)
-		return (0);
-	d = dest + n - 1;
-	s = src + n - 1;
-	while (n-- > 0)
-		*d-- = *s--;
-	return (d);
-}
-
-void	*ft_memmove(void *dest, const void*src, size_t n)
-{
-	if (!dest || !src)
-		return (0);
-	if (dest < src)
-		ft_memcpy(dest, src, n);
-	else
-		ft_revmemcpy(dest, src, n);
+	d = dest;
+	if (d < s)
+		while (n--)
+			*d++ = *s++;
+	else if (d > s)
+	{
+		d += n;
+		s += n;
+		while (n--)
+			*--d = *--s;
+	}
 	return (dest);
 }
