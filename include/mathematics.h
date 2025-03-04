c3880dd83a779e6722193bcfbe22089e99c20034
Philipp Schneider <pschneid@student.42berlin.de>
2025-03-04 14:46:22 +0100

    fix mathematics.h header

--- include/mathematics.h
+++ include/mathematics.h
@@ -6,12 +6,12 @@
 /*   By: pschneid <pschneid@student.42berl...>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 19:02:56 by pschneid          #+#    #+#             */
-/*   Updated: 2025/03/04 13:56:17 by pschneid         ###   ########.fr       */
+/*   Updated: 2025/03/04 13:57:05 by pschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MATHEMATICS_H
-#define MATHEMATICS_H
+# define MATHEMATICS_H
 
 float			abs_f(float x);
 int				abs_i(int x);
@@ -19,5 +19,3 @@ unsigned int	factorial(unsigned int n);
 int				sign_i(int x);
 
 #endif
-
-# define FT_MATH_H
